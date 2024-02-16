0a2a0ad705e3b3265a88d2aab70151e1389b9917
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-16 17:06:26 -0600

    Update ambiguityDetection.py
    Created grammar parsing algorithm

--- ambiguityDetection.py
+++ ambiguityDetection.py
@@ -3,11 +3,32 @@
 #Import the grammar
 
 def importGrammar(fileName):
-    grammar = open("grammars/" + fileName,"r")
-    print(grammar.read())
+    grammarFile = open("grammars/" + fileName,"r")
+    grammar = dict()
+    for line in grammarFile:
+        rule = parseGrammarRule(line)
+        grammar.update(rule)
+    return grammar
+
+
+def parseGrammarRule(lineOfGrammar):
+    splitLHSandRHS = lineOfGrammar.split("->")
+    if len(splitLHSandRHS) != 2:
+        raise Exception("Grammar in unexpected format")
+    
+    grammarRule = dict()
+    combinationResult = splitLHSandRHS[0].strip()
+    combinations = splitLHSandRHS[1].split("|")
+
+    for combination in combinations:
+        grammarRule[combination.strip("' \n")] = combinationResult
+        #print(combination.strip() + ":" + grammarRule[combination.strip()])
+    return grammarRule    
+
+grammar = importGrammar("grouchoGrammar.txt")
+
     #Create a dictionary using left and right sides for value and key, respectively (maybe...)
      
-
 #Step 1: Loop through sentence and replace all of the words with their linguistic abstraction
 #Step 2: We check each element in the list (each current unit of abstraction)
 #   For each unit, we mark down how many combinations it is applicable to in the grammar against the current list
@@ -17,4 +38,6 @@ def importGrammar(fileName):
 #Step 3: Create a new list and run through the loop again
 #Step 4: If our list has only one element (meaning we've created an unambiguous sentence) then we quit loop and return success
     
-importGrammar("grouchoGrammar.txt")
+#We have to loop through all keys in the dictionary
+    
+
