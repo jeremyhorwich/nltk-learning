c3d626d41622ee9bdf96591d16d5307a69cf1deb
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-26 10:20:24 -0600

    Update posTagging.py
    Updating pseudocodeUpdating algorithm based on information read in Jurafsky and Martin

--- posTagging.py
+++ posTagging.py
@@ -7,15 +7,14 @@
 #   Track the frequency of each n-gram (prob with dictionary where dict[n-gram] = frequency
 #   Look into Counter from collections
 #   At the end of the loop, save the most common 100 to a dictionary
-
 #Also count the most common unigram so we have a default value to fall back on
+#Give us the option to export the results
 
 #Make predictions using trained model
 #Tokenize the test data
 #Convert as many of the unigrams as possible into POS using data from above
-#Loop through the test data
-#   Using the trigrams, convert as many to POS as possible
-#   When no more trigrams are possible, convert as many to bigrams as possible
-#   When no more bigrams are possible, use our default value
-#       Should test one of two ways: Either we keep looping until no more n-gram is possible, then move onto n-1. OR do each type once.
-#   Afterwards, compare results against NLTK's POS tagger and spit out our accuracy
+#Loop through the test data. For each word:
+#   If our next word is part of a known trigram, use that
+#   Otherwise if our next word is part of a known bigram, use that
+#   Otherwise use our default value
+#Afterwards, compare results against NLTK's POS tagger and spit out our accuracy
