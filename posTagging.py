27bdbee0a02a55cee0181795cadce7d34351e9e5
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-26 18:30:14 -0600

    Update posTagging.py
    Created filterUsefulNGrams function

--- posTagging.py
+++ posTagging.py
@@ -4,9 +4,12 @@ from collections import Counter
 
 #Train using existing data
 
-def trainModel():
-    #Get sample data (in two parts as described below)
+def trainModel(mostCommonThreshold):
+    unigramDefinitions, posCorpus = getSampleData()
+    mostCommonUnigrams = Counter(unigramDefinitions).most_common(mostCommonThreshold)
     #Clean 100 most common (w,t pairings)
+    mostCommonBigrams = getMostFrequestNGrams(posCorpus,mostCommonThreshold,2)
+    mostCommonTrigrams = getMostFrequestNGrams(posCorpus,mostCommonThreshold,3)
     #Clean bigram and trigram pairings
     #Return: 100 most common of (unigram -> POS), (bigram), (trigram)
     pass
@@ -23,8 +26,6 @@ Looks like we need this information two ways:
 (2) For (w1,t1),(w2,t2),...,(wN,tN) we need (t1,t2,...tN)
 '''
 
-getSampleData()
-
 def getMostFrequestNGrams(tokenizedCorpus: list[str], mostCommonThreshold: int, nGram: int) -> list[str]:
     modifiedCorpus = ["." for i in range(0,nGram - 1)]      #Padding the beginning so we count how the first sentence starts
     for token in tokenizedCorpus:
@@ -36,11 +37,19 @@ def getMostFrequestNGrams(tokenizedCorpus: list[str], mostCommonThreshold: int, 
     parsedNGrams = list(ngrams(modifiedCorpus),nGram)
     return Counter(parsedNGrams).most_common(mostCommonThreshold)
 
-def createPOSDefinitionsFromNGrams(nGrams: list) -> list:
-    #Discard any entries which describe the end of a sentence (any whose definition is a period)
-    #Need to handle case of gram with more than one definition (take definition with greater frequency - just check
-    #if it is already in the return dictionary and compare its frequency with that in the return)
-    pass
+def filterUsefulNGrams(nGramsWithCounts: list[str]) -> list:
+    filteredNGramsWithCounts = dict()
+    for gram in nGramsWithCounts:
+        if not gram[-1].isalpha():
+            continue
+        if gram[:-1] in filteredNGramsWithCounts:
+            frequencyOfPotentialReplacement = nGramsWithCounts[gram]
+            frequencyOfExistingDefinition = nGramsWithCounts[filteredNGramsWithCounts[gram[:-1]]][1]
+            if frequencyOfPotentialReplacement > frequencyOfExistingDefinition:
+                filteredNGramsWithCounts[gram[:-1]] = [gram[-1],nGramsWithCounts[gram]]
+    #We don't need the counts anymore
+    cleanNGramList = {nGram:filteredNGramsWithCounts[nGram][0] for nGram in filteredNGramsWithCounts}
+    return cleanNGramList
 
 
 #Also count the most common unigram so we have a default value to fall back on
