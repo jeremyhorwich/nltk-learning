88e286f43537b74e7afdf182c3ceecfc3f06a7ec
jeremyhorwich <jeremyahorwich@gmail.com>
2024-02-27 13:17:20 -0600

    Update posTagging.py
    Creating placeholder functions for model usage

--- posTagging.py
+++ posTagging.py
@@ -50,11 +50,21 @@ def filterUsefulNGrams(nGramsWithCounts: list[str]) -> dict:
 
 #Give us the option to export the results
 
-#Make predictions using trained model
-#Tokenize the test data
-#Convert as many of the unigrams as possible into POS using data from above
-#Loop through the test data. For each word:
-#   If our next word is part of a known trigram, use that
-#   Otherwise if our next word is part of a known bigram, use that
-#   Otherwise use our default value
-#Afterwards, compare results against NLTK's POS tagger and spit out our accuracy
+def testModel():
+    pass
+
+def getTestData():
+    #Tokenize the test data
+    pass
+
+def tagWords():
+    #Convert as many of the unigrams as possible into POS using data from above
+    #Loop through the test data. For each word:
+    #   If our next word is part of a known trigram, use that
+    #   Otherwise if our next word is part of a known bigram, use that
+    #   Otherwise use our default value
+    pass
+
+def calculateAccuracyOfTags():
+    #Afterwards, compare results against NLTK's POS tagger and spit out our accuracy
+    pass
